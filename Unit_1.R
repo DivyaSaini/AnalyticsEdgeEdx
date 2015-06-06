89755212f8b2cf55b301845b88f31c3c38514078
DivyaSaini <divya.bitsgoa@gmail.com>
2015-06-06 23:09:08 +0530

    Update Unit_1.R

--- Unit_1.R
+++ Unit_1.R
@@ -1,7 +1,7 @@
-#Direct calculations
+# Direct calculations
 8*6 # 48
 2^4 # 16
-#Mathematical functions
+# Mathematical functions
 sqrt(2) # 1.414214
 abs(-65) # 65
 ?sqrt # Help pages is opened for sqrt function
@@ -12,3 +12,9 @@ HoursYear <- 365 * 24 # Another of assigning variables
 HoursYear # 8760
 # To list al the variables created in current R session
 ls() # "HoursYear" "SquareRoot2"
+
+## Vectors and Data Frames
+# To create vectors in R
+c(2,3,5,8,13) # [1] 2 3 5 8 13
+Country = c("Brazil", "China", "India", "Switzerland", "USA")
+LifeExpectancy = c(74,76,65,83,79)

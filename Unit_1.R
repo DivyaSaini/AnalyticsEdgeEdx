bad3f2b81b5c83f9937af6deebcd11caad4ee8ed
DivyaSaini <divya.bitsgoa@gmail.com>
2015-06-06 23:00:12 +0530

    Update Unit_1.R

--- Unit_1.R
+++ Unit_1.R
@@ -1,1 +1,14 @@
-
+#Direct calculations
+8*6 # 48
+2^4 # 16
+#Mathematical functions
+sqrt(2) # 1.414214
+abs(-65) # 65
+?sqrt # Help pages is opened for sqrt function
+# Saving values to a variable
+SquareRoot2 = sqrt(2) # R is case-sensitive
+SquareRoot2 # 1.414214
+HoursYear <- 365 * 24 # Another of assigning variables
+HoursYear # 8760
+# To list al the variables created in current R session
+ls() # "HoursYear" "SquareRoot2"

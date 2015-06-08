45d474ebd53a3f69de6df46c2efffe7dd0125c35
DivyaSaini <divya.bitsgoa@gmail.com>
2015-06-08 16:28:11 +0530

    Update Assignment01.R

--- Assignment01.R
+++ Assignment01.R
@@ -8,8 +8,33 @@ summary(DateConvertArrest)
 > mvtArrest$Month = months(DateConvertArrest)
 > mvtArrest$Date = DateConvertArrest
 > table(mvtArrest$Month) # JAN - 1435
+# or
+table(mvt$Arrest,mvt$Month)
 
 # 3.1 
 > DateConvert = as.Date(strptime(mvt$Date, "%m/%d/%y %H:%M"))
 > mvt$Date = DateConvert
 > hist(mvt$Date, breaks=100)
+
+## STOCK DYNAMICS
+
+# 1.1
+> setwd("~/EdxWD/data")
+> IBM = read.csv("IBMStock.csv")
+> GE = read.csv("GEStock.csv")
+> CocaCola = read.csv("CocaColaStock.csv")
+> ProcterGamble = read.csv("ProcterGambleStock.csv")
+> Boeing = read.csv("BoeingStock.csv")
+> str(IBM) # 480 obs. of  2 variables
+
+# 1.2 , 1.3
+IBM$Date = as.Date(IBM$Date, "%m/%d/%y")
+GE$Date = as.Date(GE$Date, "%m/%d/%y")
+CocaCola$Date = as.Date(CocaCola$Date, "%m/%d/%y")
+ProcterGamble$Date = as.Date(ProcterGamble$Date, "%m/%d/%y")
+Boeing$Date = as.Date(Boeing$Date, "%m/%d/%y")
+> summary(IBM)
+> summary(GE)
+> summary(CocaCola)
+> summary(ProcterGamble)
+> summary(Boeing)

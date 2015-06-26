e26980d6ada19309c937a140c4fbd0431307e068
DivyaSaini <divya.bitsgoa@gmail.com>
2015-06-26 11:03:57 -0700

    Update Unit_3.R

--- Unit_3.R
+++ Unit_3.R
@@ -4,3 +4,5 @@
 # 0 <= Logistic Response Function <= 1
 
 # Logistic response function - P(y = 1) = 1/(1 + e^(-(Bo + B1X1 + B2X2 + .... + BkXk))
+
+# Logit = log(Odds) = Bo + B1X1 + B2X2 + .... + BkXk

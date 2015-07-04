4cbbc83c85c00b51014f4c0b1cf9bd7e57aaffff
DivyaSaini <divya.bitsgoa@gmail.com>
2015-07-04 14:59:18 +0530

    Update Unit_4.R

--- Unit_4.R
+++ Unit_4.R
@@ -1,1 +1,102 @@
+# Unit 4 - "Judge, Jury, and Classifier" Lecture
+
+
+# Classification and Regression Trees (CART)
+# A yes response is always to the left and a no response is always to the right.
+# There are different ways to control how many splits are generated.
+# One way is by setting a lower bound for the number of data points in each subset.
+# In R, this is called the minbucket parameter, for the minimum number of observations in each bucket or subset.
+# The smaller minbucket is, the more splits will be generated.
+# But if it's too small, overfitting will occur.
+# This means that CART will fit the training set almost perfectly.
+# But this is bad because then the model will probably not perform well on test set data or new data.
+# On the other hand, if the minbucket parameter is too large, the model will be too simple and the accuracy will be poor.
+
+# VIDEO 4
+
+# Read in the data
+stevens = read.csv("stevens.csv")
+str(stevens)
+
+# Split the data
+library(caTools)
+set.seed(3000)
+spl = sample.split(stevens$Reverse, SplitRatio = 0.7)
+Train = subset(stevens, spl==TRUE)
+Test = subset(stevens, spl==FALSE)
+
+# Install rpart library
+install.packages("rpart")
+library(rpart)
+install.packages("rpart.plot")
+library(rpart.plot)
+
+# CART model
+StevensTree = rpart(Reverse ~ Circuit + Issue + Petitioner + Respondent + LowerCourt + Unconst, data = Train, method="class", minbucket=25)
+
+prp(StevensTree)
+
+# Make predictions
+PredictCART = predict(StevensTree, newdata = Test, type = "class")
+table(Test$Reverse, PredictCART)
+(41+71)/(41+36+22+71)
+
+# ROC curve
+library(ROCR)
+
+PredictROC = predict(StevensTree, newdata = Test)
+PredictROC
+
+pred = prediction(PredictROC[,2], Test$Reverse)
+perf = performance(pred, "tpr", "fpr")
+plot(perf)
+
+
+
+# VIDEO 5 - Random Forests
+
+# Install randomForest package
+install.packages("randomForest")
+library(randomForest)
+
+# Build random forest model
+StevensForest = randomForest(Reverse ~ Circuit + Issue + Petitioner + Respondent + LowerCourt + Unconst, data = Train, ntree=200, nodesize=25 )
+
+# Convert outcome to factor
+Train$Reverse = as.factor(Train$Reverse)
+Test$Reverse = as.factor(Test$Reverse)
+
+# Try again
+StevensForest = randomForest(Reverse ~ Circuit + Issue + Petitioner + Respondent + LowerCourt + Unconst, data = Train, ntree=200, nodesize=25 )
+
+# Make predictions
+PredictForest = predict(StevensForest, newdata = Test)
+table(Test$Reverse, PredictForest)
+(40+74)/(40+37+19+74)
+
+
+
+# VIDEO 6
+
+# Install cross-validation packages
+install.packages("caret")
+library(caret)
+install.packages("e1071")
+library(e1071)
+
+# Define cross-validation experiment
+numFolds = trainControl( method = "cv", number = 10 )
+cpGrid = expand.grid( .cp = seq(0.01,0.5,0.01)) 
+
+# Perform the cross validation
+train(Reverse ~ Circuit + Issue + Petitioner + Respondent + LowerCourt + Unconst, data = Train, method = "rpart", trControl = numFolds, tuneGrid = cpGrid )
+
+# Create a new CART model
+StevensTreeCV = rpart(Reverse ~ Circuit + Issue + Petitioner + Respondent + LowerCourt + Unconst, data = Train, method="class", cp = 0.18)
+
+# Make predictions
+PredictCV = predict(StevensTreeCV, newdata = Test, type = "class")
+table(Test$Reverse, PredictCV)
+(59+64)/(59+18+29+64)
+
 
